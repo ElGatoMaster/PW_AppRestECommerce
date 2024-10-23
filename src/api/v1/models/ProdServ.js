755e2b8c6208fa08633645e434ff7b7f0c09ecfb
Yvan Acosta <acostacarrillo.yvan@gmail.com>
2024-10-22 20:47:30 -0700

    NUEVO MODELO EDUCATIVO

--- src/api/v1/models/ProdServ.js
+++ src/api/v1/models/ProdServ.js
@@ -1,71 +1,79 @@
 import * as mongoose from 'mongoose';
 
-const prodservSchema = new mongoose.Schema({
-    IdProdServPK: { type : Number, required : true },
-    IdProdServOK: { type : String },
-    IdProdServBK: { type : String },
-    //--
-    IdProdServMaOK: { type : String },
-    IdProdServMaBK: { type : String },
-    //--
-    DesProdServ: { type : String },
-    IdMedidaOK: {  type : String },
-    IdMedidaBK: {  type : String },
-    //FIC: ESTATUS
-    cat_prod_serv_estatus: [
-      {
-            IdTipoGenEstatusOk: { type : String },
-            IdGenEstatusOk: { type : String },
-            TipoEstatus: { type : String },
-            Actual: { type: String },
-            Observacion: { type: String },
-            detail_row: {
-                FechaReg: { type: Date, default: Date.now },
-                UsuarioReg: { type: String }
-            },
-            _id: false,
-        },
-    ],
-    //FIC: ARCHIVOS
-    cat_prod_serv_archivos: [
-        {
-            DesArchivo: { type : String },
-            RutaArchivo: { type : String },
-            //FIC: Tipo Archivo
-            IdTipoGenArchivoOK: { type : String },
-            IdGenArchivoOK: { type : String },
-            TipoArchivo: { type : String },
-            //FIC: Sección
-            IdTipoGenSeccionOK: { type : String },
-            IdGenSeccionOK: { type : String },
-            TipoSeccion: { type : String },
-            //---
-            Secuencia: { type : Number },
-            Principal: { type : String },
-            detail_row: {
-                FechaReg: { type: Date, default: Date.now },
-                UsuarioReg: { type: String },
-                FechaUltMod: { type: Date, default: Date.now },
-                UsuarioMod: { type: String },
-                Activo: { type: String, default: 'S' },
-                Borrado: { type: String, default: 'N' },
-            _id: false,
-            },
-            _id: false,
-        },
-    ], 
-    detail_row: {
-          FechaReg: { type: Date, default: Date.now },
-          UsuarioReg: { type: String },
-          FechaUltMod: { type: Date, default: Date.now },
-          UsuarioMod: { type: String },
-          Activo: { type: String, default: 'S' },
-          Borrado: { type: String, default: 'N' }
-    }
+const Schema = mongoose.Schema;
+
+const DetailRowSchema = new Schema({
+  Activo: { type: String, default: 'S' },
+  Borrado: { type: String, default: 'N' },
+  detail_row_reg: [{
+    FechaReg: { type: Date, default: Date.now },
+    UsuarioReg: { type: String }
+  }]
+});
+const EstatusSchema = new Schema({
+  IdTipoEstatusOK: { type: String },
+  Actual: { type: String },
+  Observacion: { type: String },
+  detail_row: DetailRowSchema
+});
+const InfoAdSchema = new Schema({
+  IdEtiquetaOK: { type: String },
+  IdEtiqueta: { type: String },
+  Etiqueta: { type: String },
+  Valor: { type: String },
+  IdTipoSeccionOK: { type: String },
+  Secuencia: { type: Number },
+  detail_row: DetailRowSchema
+});
+const PaqueteSchema = new Schema({
+  IdPresentaOK: { type: String },
+  DesPresenta: { type: String },
+  Cantidad: { type: Number },
+  detail_row: DetailRowSchema
+});
+const ArchivoSchema = new Schema({
+  IdArchivoOK: { type: String },
+  IdArchivoBK: { type: String },
+  DesArchivo: { type: String },
+  RutaArchivo: { type: String },
+  Path: { type: String },
+  IdTipoArchivoOK: { type: String },
+  IdTipoSeccionOK: { type: String },
+  Secuencia: { type: Number },
+  Principal: { type: String },
+  detail_row: DetailRowSchema
+});
+const PresentacionSchema = new Schema({
+  IdPresentaOK: { type: String },
+  IdPresentaBK: { type: String },
+  CodigoBarras: { type: String },
+  DesPresenta: { type: String },
+  Indice: { type: String },
+  Principal: { type: String },
+  estatus: [EstatusSchema],
+  info_vta: [{ type: Schema.Types.Mixed }],
+  info_ad: [InfoAdSchema],
+  paquete: [PaqueteSchema],
+  archivos: [ArchivoSchema],
+  detail_row: DetailRowSchema
+});
+const NegocioSchema = new Schema({
+  IdNegocioOK: { type: String },
+  detail_row: DetailRowSchema
+});
+const ProdServSchema = new Schema({
+  IdInstitutoOK: { type: String },
+  IdProdServOK: { type: String },
+  IdProdServBK: { type: String },
+  CodigoBarras: { type: String },
+  DesProdServ: { type: String },
+  Indice: { type: String },
+  estatus: [EstatusSchema],
+  presentaciones: [PresentacionSchema],
+  archivos: [ArchivoSchema],
+  negocios: [NegocioSchema],
+  info_ad: [InfoAdSchema],
+  detail_row: DetailRowSchema
 });
 
-export default mongoose.model(
-    'cat_productos',
-    prodservSchema,
-    'cat_productos'
-  );
+module.exports = mongoose.model('cat_productos', ProdServSchema);
